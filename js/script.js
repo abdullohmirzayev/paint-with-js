c5819feda293305a800d6c6a5f023cd6d24d4e36
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-07 12:52:52 +0500

    added drawTriangle fun

--- js/script.js
+++ js/script.js
@@ -55,6 +55,16 @@ const drawCircle = (e) => {
   fillColor.checked ? ctx.fill() : ctx.stroke();
 };
 
+// DRAW TRIANGLE
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prevMouseX, prevMouseY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+  ctx.closePath();
+  fillColor.checked ? ctx.fill() : ctx.stroke();
+};
+
 // DRAWING
 const drawing = (e) => {
   if (!isDrawing) return;
@@ -70,6 +80,9 @@ const drawing = (e) => {
       break;
     case "circle":
       drawCircle(e);
+      break;
+    case "triangle":
+      drawTriangle(e);
       break;
     default:
       break;
